0c79558a0dd5daee581b1f2c74f988315bb2cb3b
jojimenez78 <garciaalonso527@gmail.com>
2025-04-13 22:53:21 -0600

    header-fix

--- logic.js
+++ logic.js
@@ -3,20 +3,20 @@ document.addEventListener("DOMContentLoaded", function () {
     const buttons = document.querySelectorAll(".card__button");
     const closeBtn = document.querySelector(".close");
   
-    // Mostrar el modal al hacer clic en el botón
+    
     buttons.forEach(button => {
       button.addEventListener("click", function (e) {
-        e.preventDefault(); // Prevenir navegación
+        e.preventDefault(); 
         modal.style.display = "block";
       });
     });
   
-    // Cerrar modal al hacer clic en la X
+   
     closeBtn.onclick = function () {
       modal.style.display = "none";
     };
   
-    // Cerrar al hacer clic fuera del modal
+  
     window.onclick = function (event) {
       if (event.target == modal) {
         modal.style.display = "none";
